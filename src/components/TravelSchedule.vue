3d5302e74b18b2168739904aa28f2881b352024d
yumi <yumisunky@gmail.com>
2025-11-12 12:57:33 +0800

    style: adjust schedule button

--- src/components/TravelSchedule.vue
+++ src/components/TravelSchedule.vue
@@ -121,13 +121,13 @@
       </div>
 
       <div class="flex justify-end gap-2">
-        <button
+        <!-- <button
           type="button"
           @click="scheduleCancel"
           class="bg-gray-300 hover:bg-gray-200 px-4 py-2 rounded"
         >
           取消
-        </button>
+        </button> -->
         <button
           type="submit"
           :disabled="isLoading"
